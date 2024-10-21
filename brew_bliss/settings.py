749a46b3b3c64d5867b8c2e6f4480476fc821a7f
John Lynch <johnf.lynch@yahoo.ie>
2024-10-21 19:45:33 +0000

    feat: added base menu folders media and css

--- brew_bliss/settings.py
+++ brew_bliss/settings.py
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ao01q#2n(b6!sy!re^$a3w7al_@v0p!n3zb%#*wgdrb&0*(j47'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['8000-carlow78-brewbliss-tsq4xp953ow.ws.codeinstitute-ide.net']
--- brew_bliss/urls.py
+++ brew_bliss/urls.py
@@ -18,10 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    
-    path('', include('home.urls')),
+     
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),
     
 
 ]
