263f29b44d375c3bf4042f0eaaf8933f0cdde64e
John Lynch <johnf.lynch@yahoo.ie>
2024-10-28 18:16:46 +0000

    feat: added search function view

--- products/views.py
+++ products/views.py
@@ -1,27 +1,44 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product 
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Product
+
 
 def all_products(request):
-    # show all products
+    # show all products and search function
 
     products = Product.objects.all()
+    query = request.GET.get('q', '')
+
+    if request.GET:
+        if query:
+            search_fields = ['name', 'description']
+            search_query = Q()
+
+            for field in search_fields:
+                search_query |= Q(**{f"{field}__icontains": query})
+
+            products = products.filter(search_query)
+        else:
+            messages.error(request, "Please enter a search term.")
+            return redirect(reverse('products'))
 
     context = {
         'products': products,
-
+        'search_term': query
     }
+
 
     return render(request, 'products/products.html', context)
 
-def product_detail (request, product_id):
-    # show an individual product
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
         'product': product,
-
     }
 
     return render(request, 'products/product_detail.html', context)
-
