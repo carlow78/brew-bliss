03f72812153cc6adb49385db86ed056e5bbc1338
John Lynch <johnf.lynch@yahoo.ie>
2024-11-04 13:26:38 +0000

    feat: added weight logic

--- cart/views.py
+++ cart/views.py
@@ -11,12 +11,26 @@ def add_to_cart(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+
+    weight = None
+    if 'product_weight' in request.POST:
+        weight = request.POST['product_weight']
+
     cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
+    if weight:
+        if item_id in list(cart.keys()):
+            if weight in cart[item_id]['items_by_weight'].keys():
+                cart[item_id]['items_by_weight'][weight] += quantity
+            else:
+                cart[item_id]['items_by_weight'][weight] = quantity
+        else:
+            cart[item_id] = {'items_by_weight': {weight: quantity}}
     else:
-        cart[item_id] = quantity
+        if item_id in list(cart.keys()):
+            cart[item_id] += quantity
+        else:
+            cart[item_id] = quantity
 
     request.session['cart'] = cart
     return redirect(redirect_url)
