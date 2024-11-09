9d29123e29ff477a7e253385e18e3dfac3715a95
John Lynch <johnf.lynch@yahoo.ie>
2024-11-09 16:58:59 +0000

    feature: add update and remove item from cart functionality

--- cart/urls.py
+++ cart/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
 
     path('', views.view_cart, name='view_cart'),
     path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
+    path('modify/<int:item_id>/', views.modify_cart, name='modify_cart'),
+    path('remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
     
-
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 from products.models import Product
-
-
-# Create your views here.
+from django.http import HttpResponseRedirect
 
 
 def view_cart(request):
     """ A view that renders the cart contents page """
 
     return render(request, 'cart/cart.html')
+
 
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
@@ -25,3 +24,26 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def modify_cart(request, item_id):
+    """Adjust the quantity of the specified product in the cart"""
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id, None) 
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, item_id):
+    cart = request.session.get('cart', {})
+    if str(item_id) in cart:
+        del cart[str(item_id)]
+        request.session['cart'] = cart
+    return redirect('view_cart')
