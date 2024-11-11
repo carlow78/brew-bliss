c7a3fee8eb0256e8371654312bc45f9b443a8460
John Lynch <johnf.lynch@yahoo.ie>
2024-11-11 20:28:23 +0000

    feat: basic added stripe functionality

--- brew_bliss/settings.py
+++ brew_bliss/settings.py
@@ -199,6 +199,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Stripe settings
 FREE_DELIVERY_THRESHOLD = 50
-# STANDARD_DELIVERY = 5
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY','')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY','')
+
+
 
--- checkout/views.py
+++ checkout/views.py
@@ -1,21 +1,42 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from cart.context import cart_contents
 
+import stripe
 
 def checkout(request):
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "There's nothing in your cart at the moment")
         return redirect(reverse('products'))
 
+    current_cart = cart_contents(request)
+    total = current_cart['final_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.api_key = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print (intent)
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe PK is missing. Is it set in your env?')
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key':'pk_test_51Q19bS01JENvyaBElkBKg3TubIzVzqQ5cy0zrAubqt0CcIDAwSGcVGORe3gqeeqfblK2Hu8yYA29dss8xbEhfVfk00aJ7ZeonS',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
