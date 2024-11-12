f723c17b1379faa2e4c6487f3f20e4de5afab080
John Lynch <johnf.lynch@yahoo.ie>
2024-11-12 21:07:18 +0000

    feat: setup stripe webhook and test functionality

--- checkout/models.py
+++ checkout/models.py
@@ -6,6 +6,7 @@ from decimal import Decimal
 from django.conf import settings
 from products.models import Product
 
+from django_countries.fields import CountryField
 
 class Order(models.Model):
     """
@@ -15,7 +16,7 @@ class Order(models.Model):
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = models.CountryField(blank_label='Country *', null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
@@ -25,6 +26,8 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     final_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         
