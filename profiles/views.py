dd131b06d2dc45724f09cfc526be7d0cd39ff10d
John Lynch <johnf.lynch@yahoo.ie>
2024-11-13 21:01:44 +0000

    feat: added order history and updated checkout views

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
+
 def profile(request):
    # Display user profile
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -19,6 +21,18 @@ def profile(request):
     context = {
          'form': form,
          'orders': orders,
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
