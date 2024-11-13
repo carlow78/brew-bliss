545e358d21af34e23bd5fbe5da6c7ea0cf5868b7
John Lynch <johnf.lynch@yahoo.ie>
2024-11-13 12:10:10 +0000

    fix: success error message when updating qty of existing item already in cart add_to_cart view

--- cart/views.py
+++ cart/views.py
@@ -13,17 +13,19 @@ def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
 
     product = get_object_or_404(Product, pk=item_id)
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
+    quantity = int(request.POST.get('quantity', 1))  
+    redirect_url = request.POST.get('redirect_url', '/') 
     cart = request.session.get('cart', {})
 
-   
-    if item_id in list(cart.keys()):
-            cart[item_id] += quantity
-            messages.success(request, f'Updated {product.name} quantity to {item_id}')
+    if item_id in cart:
+        cart[item_id] += quantity
+        messages.success(
+            request, 
+            f'Updated {product.name} quantity to {cart[item_id]}'  # Corrected this line
+        )
     else:
-            cart[item_id] = quantity
-            messages.success(request, f'Successfully added {product.name} to your cart!')
+        cart[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
