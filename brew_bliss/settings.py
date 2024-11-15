7a4f47e40472b9d215a67cc280959d8b90c8e66c
John Lynch <johnf.lynch@yahoo.ie>
2024-11-15 21:38:53 +0000

    setting update

--- brew_bliss/settings.py
+++ brew_bliss/settings.py
@@ -17,8 +17,8 @@ import dj_database_url
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 if os.path.exists('env.py'):
     import env
@@ -153,8 +153,26 @@ WSGI_APPLICATION = 'brew_bliss.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
+if 'DATABASE_URL' in os.environ:
+
+    DATABASES = {
+
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+
+    }
+
+else:
+
+    DATABASES = {
+
+        'default': {
+
+            'ENGINE': 'django.db.backends.sqlite3',
+
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        }
+
     }
 
 
