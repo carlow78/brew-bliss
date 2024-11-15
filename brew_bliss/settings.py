2be09d1e97cf8a3ad53425dc14dac0f3c299f3c9
John Lynch <johnf.lynch@yahoo.ie>
2024-11-15 16:13:47 +0000

    settings update

--- brew_bliss/settings.py
+++ brew_bliss/settings.py
@@ -217,7 +217,7 @@ if 'USE_AWS' in os.environ:
    
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+    AWS_STORAGE_BUCKET_NAME = 'brew-bliss2'
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_REGION_NAME = 'eu-west-1'
 
