6a7f6ab9025a13529ade9744d46a8dc880346987
John Lynch <johnf.lynch@yahoo.ie>
2024-11-15 22:33:09 +0000

    aws update

--- brew_bliss/settings.py
+++ brew_bliss/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 import os
 import dj_database_url
-import django_heroku
+from django.conf import settings
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
@@ -233,7 +234,8 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.eu-west-1.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = 'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/'
+    AWS_S3_SIGNATURE_VERSION = 's3v4'
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
