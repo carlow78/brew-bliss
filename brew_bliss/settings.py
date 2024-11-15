2255b6d3525c665ebb159da58eeae4ee17fc584d
John Lynch <johnf.lynch@yahoo.ie>
2024-11-15 14:05:42 +0000

    aws settings update

--- brew_bliss/settings.py
+++ brew_bliss/settings.py
@@ -16,7 +16,8 @@ import os
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if os.path.exists('env.py'):
     import env
