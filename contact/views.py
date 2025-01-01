c5ecde500bac4026f30b090bffa965eae5bd7b00
John Lynch <johnf.lynch@yahoo.ie>
2025-01-01 17:44:33 +0000

    feat: add heroimage and css to success

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from .forms import ContactUsForm
-from .utils import send_notification
 
 def contact(request):
 
@@ -10,7 +9,6 @@ def contact(request):
         form = ContactUsForm(request.POST)
         if form.is_valid():
             contact = form.save()
-            send_notification(contact)  # Send notification email
             return redirect('contactsuccess')  # Redirect to a success page
     
     return render(request, 'contact/contact.html', {'form': form})
