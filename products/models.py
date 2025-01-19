c8718040f173d412d9470c62b06b81b91aea0cb2
John Lynch <johnf.lynch@yahoo.ie>
2025-01-19 13:34:50 +0000

    fix: add like_counts field to product model to resolve adding new item to store

--- products/models.py
+++ products/models.py
@@ -22,9 +22,8 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    likes_count = models.IntegerField(default=0)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2,
-                                 null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
